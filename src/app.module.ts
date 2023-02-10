61579b9ecc6c729ac10b1a01de7d6939ac697a25
Federico Castelau <castelaufederico@gmail.com>
2023-02-10 19:42:40 -0300

    create market controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocketModule } from './socket/socket.module';
 import { OrderBookModule } from './order-book/order-book.module';
+import { MarketController } from './market/market.controller';
 
 @Module({
   imports: [SocketModule, OrderBookModule],
-  controllers: [AppController],
+  controllers: [AppController, MarketController],
   providers: [AppService],
 })
 export class AppModule {}
