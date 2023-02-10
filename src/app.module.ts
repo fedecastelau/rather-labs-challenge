83f3144c4d6f074d19d0bf7e3baf18b39d72d9bd
Federico Castelau <castelaufederico@gmail.com>
2023-02-10 19:54:46 -0300

    fixed app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocketModule } from './socket/socket.module';
 import { OrderBookModule } from './order-book/order-book.module';
-import { MarketController } from './market/market.controller';
 
 @Module({
   imports: [SocketModule, OrderBookModule],
-  controllers: [AppController, MarketController],
+  controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/order-book/order-book.module.ts
+++ src/order-book/order-book.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderBookService } from './order-book.service';
+import { OrderBookController } from './order-book.controller';
 
 @Module({
   providers: [OrderBookService],
   exports: [OrderBookService],
+  controllers: [OrderBookController],
 })
 export class OrderBookModule {}
